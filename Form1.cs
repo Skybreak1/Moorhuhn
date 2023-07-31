62836ef6a851645c3fabcb47cc103ac904c20cab
Skybreak1 <133328162+Skybreak1@users.noreply.github.com>
2023-07-31 11:22:58 +0200

    Kommentare zum Code

--- Form1.cs
+++ Form1.cs
@@ -9,156 +9,164 @@ namespace Moorhuhn
 {
     public partial class Form1 : Form
     {
-        private int timerInt = 600;
-        private int huhnCounter = 0;
-        private List<PictureBox> pictureBoxList = new List<PictureBox>();
-        private static System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
-        private static System.Windows.Forms.Timer timerZeit = new System.Windows.Forms.Timer();
-        private int Zeit = 60;
-        private Label timerLabel;
-        private int gesamtscore;
+        private int timerInt = 600; // Initialisierung einer Variablen für einen Timer-Zähler
+        private int huhnCounter = 0; // Initialisierung einer Variablen zur Zählung der Hühner
+        private List<PictureBox> pictureBoxList = new List<PictureBox>(); // Initialisierung einer Liste für PictureBox-Objekte
+        private static System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer(); // Initialisierung eines Timer-Objekts für das Spiel
+        private static System.Windows.Forms.Timer timerZeit = new System.Windows.Forms.Timer(); // Initialisierung eines Timer-Objekts zur Zeitmessung
+        private int Zeit = 60; // Initialisierung einer Variablen für die verbleibende Zeit im Spiel
+        private Label timerLabel; // Initialisierung eines Labels für die Anzeige der verbleibenden Zeit
+        private int gesamtscore; // Initialisierung einer Variablen für den Gesamtscore des Spielers
 
         public Form1()
         {
-            InitializeComponent();
-            SetBackgroundImage();
+            InitializeComponent(); // Initialisierung der Form-Komponenten
+            SetBackgroundImage(); // Festlegen des Hintergrundbilds der Form
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            timer.Tick += new EventHandler(GameLoop);
-            timer.Interval = 10; // in ms
-            timer.Start();
+            timer.Tick += new EventHandler(GameLoop); // Hinzufügen einer Methode (GameLoop) zum Tick-Event des Timers
+            timer.Interval = 10; // Setzen des Timer-Intervalls auf 10 Millisekunden (für das GameLoop-Update)
+            timer.Start(); // Starten des Timers
 
-            timerZeit.Tick += new EventHandler(ZeitMessung);
-            timerZeit.Interval = 1000;
-            timerZeit.Start();
+            timerZeit.Tick += new EventHandler(ZeitMessung); // Hinzufügen einer Methode (ZeitMessung) zum Tick-Event des Zeitmessungs-Timers
+            timerZeit.Interval = 1000; // Setzen des Timer-Intervalls auf 1 Sekunde (für die Zeitmessung)
+            timerZeit.Start(); // Starten des Zeitmessungs-Timers
         }
 
         private void GameLoop(object myObject, EventArgs myEventArgs)
         {
-            timerInt++;
+            timerInt++; // Erhöhen des Timer-Zählers
 
-            if (timerInt % 100 == 0) // Jede x Sekunden Spawn Huhn
+            if (timerInt % 100 == 0) // Alle 100 Einheiten des Timer-Zählers ein Huhn spawnen
             {
-                SpawnHuhn();
+                SpawnHuhn(); // Methode zum Erzeugen eines neuen Huhns aufrufen
             }
 
-            for (int i = pictureBoxList.Count - 1; i >= 0; i--)
+            for (int i = pictureBoxList.Count - 1; i >= 0; i--) // Schleife zum Bewegen der vorhandenen Hühner auf dem Bildschirm
             {
-                PictureBox pictureBox = pictureBoxList[i];
-                pictureBox.Location = new Point(pictureBox.Location.X + 7, pictureBox.Location.Y);
-                if (pictureBox.Location.X > 2500)
+                PictureBox pictureBox = pictureBoxList[i]; // Das aktuelle PictureBox-Objekt aus der Liste
+                pictureBox.Location = new Point(pictureBox.Location.X + 7, pictureBox.Location.Y); // Verschieben des Huhns nach rechts
+
+                if (pictureBox.Location.X > 2500) // Wenn das Huhn den rechten Rand des Bildschirms erreicht
                 {
-                    // Remove the PictureBox from the list and the form's controls
+                    // Huhn aus der Liste und den Controls der Form entfernen und das PictureBox-Objekt freigeben
                     pictureBoxList.RemoveAt(i);
                     this.Controls.Remove(pictureBox);
                     pictureBox.Dispose();
                 }
             }
 
-            //Debug.WriteLine(Cursor.Position.ToString());
+            //Debug.WriteLine(Cursor.Position.ToString()); // Ausgabe der aktuellen Mausposition (Debugging-Zwecke)
         }
 
         private void ZeitMessung(object myObject, EventArgs myEventArgs)
         {
-            Zeit--;
-            lblZeit.Text = Zeit.ToString();
+            Zeit--; // Verringern der verbleibenden Zeit
+            lblZeit.Text = Zeit.ToString(); // Aktualisieren der Anzeige für die verbleibende Zeit
 
-            if (Zeit <= 0)
+            if (Zeit <= 0) // Wenn die Zeit abgelaufen ist
             {
-                timerZeit.Stop();
-                timer.Stop();
-                ShowResultMessage();
+                timerZeit.Stop(); // Stopp des Zeitmessungs-Timers
+                timer.Stop(); // Stopp des Haupt-Spiel-Timers
+                ShowResultMessage(); // Anzeige einer Ergebnismeldung
             }
         }
+
         private void ShowResultMessage()
         {
+            // Anzeigen einer MessageBox mit dem Spielergebnis
             MessageBox.Show("Die Zeit ist vorbei! Dein Score: " + gesamtscore.ToString(), "Ergebnisse", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
         private void SpawnHuhn()
         {
-            //Debug.WriteLine("Spawne Huhn als button");
-            PictureBox pictureBox = new PictureBox();
-            huhnCounter++;
-            pictureBox.Name = "Butt" + huhnCounter;
+            // Methode zum Erzeugen eines neuen Huhns (PictureBox)
 
+            PictureBox pictureBox = new PictureBox(); // Erzeugen eines neuen PictureBox-Objekts
+            huhnCounter++; // Zähler für die Hühner erhöhen
+            pictureBox.Name = "Butt" + huhnCounter; // Name des PictureBox-Objekts setzen (Butt1, Butt2, ...)
 
-            //button.Text = "New";
+            pictureBox.Location = new Point(-100, new Random().Next(400)); // Position des Huhns setzen (Start außerhalb des Bildschirms, zufällige Y-Position)
+            int score = new Random().Next(20, 50); // Zufälliger Punktwert für das Huhn
+            pictureBox.Size = new Size(score, score); // Größe der PictureBox entsprechend des Punktwerts
+            pictureBox.Tag = score; // Punktwert des Huhns in das Tag-Attribut der PictureBox schreiben
+            pictureBox.Image = Image.FromFile(@"..\\..\\..\\Bilder\\huhn.gif"); // Laden des Huhn-Bildes aus einer Datei
+            pictureBox.BackColor = Color.Transparent; // Transparente Hintergrundfarbe für die PictureBox
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage; // Skalieren des Bildes auf die PictureBox-Größe
+            pictureBox.BorderStyle = BorderStyle.None; // Keine Rahmenlinie um die PictureBox anzeigen
 
-            pictureBox.Location = new Point(-100, new Random().Next(400));
-            int score = new Random().Next(20, 50);
-            pictureBox.Size = new Size(score, score);
-            pictureBox.Tag = score;
-            pictureBox.Image = Image.FromFile(@"..\\..\\..\\Bilder\\huhn.gif");
-            pictureBox.BackColor = Color.Transparent;
-            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox.BorderStyle = BorderStyle.None;
-            //pictureBox.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
-
-            this.Controls.Add(pictureBox);
-            pictureBoxList.Add(pictureBox);
-            // add click event to the button.
-            pictureBox.Click += new EventHandler(NewPictureBox_Click);
-            //Debug.WriteLine(Cursor.Position.ToString());
+            this.Controls.Add(pictureBox); // Hinzufügen der PictureBox zum Formular
+            pictureBoxList.Add(pictureBox); // Hinzufügen der PictureBox zum Huhn-Liste
+            pictureBox.Click += new EventHandler(NewPictureBox_Click); // Hinzufügen eines Click-Events für die PictureBox
+            //Debug.WriteLine(Cursor.Position.ToString()); // Ausgabe der aktuellen Mausposition (Debugging-Zwecke)
         }
 
         // In event method.
         private async void NewPictureBox_Click(object sender, EventArgs e)
         {
-            //Debug.WriteLine("Click!");
-            PictureBox pictureBox = (PictureBox)sender;
-            pictureBox.Image = Image.FromFile(@"..\\..\\..\\Bilder\\huhn1.png");
-            await Task.Delay(200);
-            gesamtscore += Convert.ToInt16(pictureBox.Tag);
-            this.Controls.Remove(pictureBox);
-            pictureBox.Dispose();
-            lblPunktzahl.Text = gesamtscore.ToString();
+            // Methode zum Behandeln des Klick-Events einer PictureBox (wenn der Spieler ein Huhn trifft)
+
+            //Debug.WriteLine("Click!"); // Ausgabe einer Debug-Nachricht (Debugging-Zwecke)
+            PictureBox pictureBox = (PictureBox)sender; // Das auslösende PictureBox-Objekt des Klick-Events
+            pictureBox.Image = Image.FromFile(@"..\\..\\..\\Bilder\\huhn1.png"); // Ändern des Huhn-Bildes auf ein getroffenes Huhn
+            await Task.Delay(200); // Kurze Pause (Animation für das getroffene Huhn)
+
+            gesamtscore += Convert.ToInt16(pictureBox.Tag); // Erhöhen des Gesamtscores entsprechend dem Punktwert des getroffenen Huhns
+            this.Controls.Remove(pictureBox); // Entfernen der PictureBox vom Formular
+            pictureBox.Dispose(); // Freigeben des PictureBox-Objekts
+            lblPunktzahl.Text = gesamtscore.ToString(); // Aktualisieren der Anzeige für die Punktzahl des Spielers
         }
 
         private void SetBackgroundImage()
         {
-            string imagePath = @"..\\..\\..\\Bilder\\hintergrund.jpg";
-            if (System.IO.File.Exists(imagePath))
+            // Methode zum Setzen des Hintergrundbilds der Form
+
+            string imagePath = @"..\\..\\..\\Bilder\\hintergrund.jpg"; // Pfad zum Hintergrundbild
+
+            if (System.IO.File.Exists(imagePath)) // Überprüfen, ob die Bilddatei existiert
             {
                 try
                 {
                     using (FileStream stream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
                     {
-                        BackgroundImage = Image.FromStream(stream);
-                        BackgroundImageLayout = ImageLayout.Stretch;
+                        BackgroundImage = Image.FromStream(stream); // Laden des Bildes und Setzen als Hintergrundbild
+                        BackgroundImageLayout = ImageLayout.Stretch; // Stretch-Layout für das Hintergrundbild (es wird an die Größe der Form angepasst)
                     }
                 }
                 catch (Exception ex)
                 {
-                    Debug.WriteLine("Fehler beim Laden des Hintergrundbildes: " + ex.Message);
+                    Debug.WriteLine("Fehler beim Laden des Hintergrundbildes: " + ex.Message); // Ausgabe einer Fehlermeldung (Debugging-Zwecke)
                 }
             }
             else
             {
-                Debug.WriteLine("Hintergrundbild nicht gefunden: " + imagePath);
+                Debug.WriteLine("Hintergrundbild nicht gefunden: " + imagePath); // Ausgabe einer Fehlermeldung, wenn das Bild nicht gefunden wurde (Debugging-Zwecke)
             }
         }
 
         private void Restart_Click(object sender, EventArgs e)
         {
-            // Reset the game-related variables
+            // Methode zum Neustart des Spiels
+
+            // Zurücksetzen der spielrelevanten Variablen
             timerInt = 600;
             huhnCounter = 0;
             gesamtscore = 0;
             Zeit = 60;
-            lblPunktzahl.Text = gesamtscore.ToString();
-            lblZeit.Text = Zeit.ToString();
+            lblPunktzahl.Text = gesamtscore.ToString(); // Zurücksetzen der Punktzahl-Anzeige
+            lblZeit.Text = Zeit.ToString(); // Zurücksetzen der Zeit-Anzeige
 
-            // Clear existing PictureBoxes
+            // Entfernen der vorhandenen PictureBoxes
             foreach (var pictureBox in pictureBoxList)
             {
                 this.Controls.Remove(pictureBox);
                 pictureBox.Dispose();
             }
-            pictureBoxList.Clear();
+            pictureBoxList.Clear(); // Leeren der PictureBox-Liste
 
-            // Restart the timers
+            // Wiederstarten der Timer
             timerZeit.Start();
             timer.Start();
         }
